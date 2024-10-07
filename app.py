15212393006b0d913a8096a2232407e9cee6295e
Minh12344321 <2251272729@e.tlu.edu.vn>
2024-10-07 22:14:45 +0700

    update

--- app.py
+++ app.py
@@ -1,77 +1,71 @@
 from flask import Flask, render_template, request
-import pandas as pd
-import numpy as np
 import joblib
-from sklearn.linear_model import LinearRegression, Lasso
-from sklearn.neural_network import MLPRegressor
-from sklearn.preprocessing import StandardScaler
+import numpy as np
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
-# Đường dẫn đến các mô hình đã huấn luyện
-linear_model_path = "models/linear_model.pkl"
-lasso_model_path = "models/lasso_model.pkl"
-mlp_model_path = "models/mlp_model.pkl"
-stacking_model_path = "models/stacking_model.pkl"
-scaler_path = "models/scaler.pkl"
+file_path1 = 'models/linear.pkl'
+file_path2 = 'models/lasso.pkl'
+file_path3 = 'models/mlp.pkl'
+file_path4 = 'models/stacking.pkl'
 
-# Tải các mô hình đã lưu
-linear_model = joblib.load(linear_model_path)
-lasso_model = joblib.load(lasso_model_path)
-mlp_model = joblib.load(mlp_model_path)
-stacking_model = joblib.load(stacking_model_path)
-scaler = joblib.load(scaler_path)
+# Kiểm tra xem các file mô hình có tồn tại không
+for file_path in [file_path1, file_path2, file_path3, file_path4]:
+    if not os.path.exists(file_path):
+        print(f"File '{file_path}' does not exist!")
 
-# Trang chủ (hiển thị form nhập liệu)
+model = joblib.load(file_path1)
+model2 = joblib.load(file_path2)
+model3 = joblib.load(file_path3)
+model4 = joblib.load(file_path4)
+
+# Kiểm tra loại của từng mô hình
+print(f"Model type (Linear): {type(model)}")
+print(f"Model type (Lasso): {type(model2)}")
+print(f"Model type (MLP): {type(model3)}")
+print(f"Model type (Stacking): {type(model4)}")
+
+# Load the real_estate data (if needed)
+real_estate_data = pd.read_csv('Real estate price.csv')
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Xử lý form và dự đoán BMI
-@app.route('/prediction', models=['POST'])
-def prediction():
-    # Lấy dữ liệu từ form
-    date = float(request.form['transaction_date'])
-    age = float(request.form['house_age'])
-    mrt = float(request.form['mrt_distance'])
-    store = float(request.form['stores'])
-    latitude= float(request.form['longitude'])
-    longitude = float(request.form['latitude'])
-    price= float(request.form['price'])
-    model= request.form['model']
-    
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        # Get form data
+        date = request.form['date']
+        age = float(request.form['age'])
+        distance = float(request.form['distance'])
+        store = float(request.form['store'])
+        latitude = float(request.form['latitude'])
+        longitude = float(request.form['longitude'])
 
-    # Tính toán BMI dựa trên chiều cao và cân nặng
-    input_data = np.array([[date, age,mrt,store,latitude,longitude,price]])
+        # In ra các giá trị đầu vào
+        print(f"Date: {date}, Age: {age}, Distance: {distance}, Store: {store}, Latitude: {latitude}, Longitude: {longitude}")
 
-    # Chuẩn hóa dữ liệu cho Neural Network và Stacking
-    input_data_scaled = scaler.transform(input_data)
+        # Create feature array for prediction
+        features = np.array([[date, age, distance, store, latitude, longitude]])
 
-    # Chọn mô hình dự đoán
-    if model == 'LinearRegression':
-        prediction = linear_model.predict(input_data)
-    elif model == 'lassoRegressionn':
-        prediction = lasso_model.predict(input_data)
-    elif model == 'NeuralNetwork':
-        prediction = mlp_model.predict(input_data_scaled)
-    elif model == 'Stacking':
-        prediction = stacking_model.predict(input_data_scaled)
+        # Predict using the loaded model
+        print("Starting prediction...")
+        prediction = model.predict(features)
+        prediction2 = model2.predict(features)
+        prediction3 = model3.predict(features)
+        prediction4 = model4.predict(features)
 
-    # Làm tròn kết quả dự đoán
-    bmi_value = round(prediction[0], 2)
+        # In ra các giá trị dự đoán
+        print(f"Prediction 1: {prediction[0]}")
+        print(f"Prediction 2: {prediction2[0]}")
+        print(f"Prediction 3: {prediction3[0]}")
+        print(f"Prediction 4: {prediction4[0]}")
 
-    # Phân loại BMI
-    if bmi_value < 18.5:
-        bmi_category = "Thiếu cân (Khô lâu đại tướng 💀)"
-    elif 18.5 <= bmi_value < 24.99:
-        bmi_category = "Cân nặng bình thường (Good 👍)"
-    elif 25 <= bmi_value < 29.99:
-        bmi_category = "Thừa cân (Fat man 🐽)"
-    else:
-        bmi_category = "Béo phì (Hốc trưởng 🍴)"
-
-    # Truyền lại các giá trị đã nhập và kết quả dự đoán vào template
-    return render_template('index.html', bmi=bmi_value, category=bmi_category,date = date,age = age,mrt = mrt,store = store,latitude = latitude,longitude = longitude,price = price ,model=model)
+        # Return the prediction result
+        return render_template('index.html', prediction=prediction[0], prediction2=prediction2[0], prediction3=prediction3[0], prediction4=prediction4[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ train_models.py
@@ -0,0 +1,136 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import Lasso
+from sklearn.neural_network import MLPRegressor 
+from sklearn.ensemble import StackingRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.ensemble import  StackingRegressor
+import pandas as pd
+import numpy as np
+import joblib
+import os
+
+
+# Đọc dữ liệu từ file csv
+file_path = 'Real estate price.csv'
+real_estate_data = pd.read_csv(file_path)
+print(real_estate_data)
+
+# Bỏ cột "Date/Time"
+real_estate_data_cleaned = real_estate_data.drop(columns=['No'])
+
+X = real_estate_data_cleaned.drop(columns=['Y house price of unit area'])
+y = real_estate_data_cleaned['Y house price of unit area']
+
+# Chia tập dữ liệu thành tập huấn luyện và tập kiểm tra (80% huấn luyện, 20% kiểm tra)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=100)
+
+# Hiển thị dữ liệu đã được làm sạch và hình dạng của tập huấn luyện/kiểm tra
+(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+
+# Khởi tạo mô hình hồi quy tuyến tính
+linear_model = LinearRegression()
+
+
+# Huấn luyện mô hình
+linear_model.fit(X_train, y_train)
+
+# Dự đoán trên tập kiểm tra
+y_pred_linear = linear_model.predict(X_test)
+
+
+# Đánh giá mô hình
+mse_linear = mean_squared_error(y_test, y_pred_linear)
+rmse_linear = np.sqrt(mse_linear)
+mae_linear = mean_absolute_error(y_test, y_pred_linear)
+r2_linear = r2_score(y_test, y_pred_linear)
+print(f"Linear Regression MSE: {mse_linear}")
+print(f"Linear Regression R-square: {r2_linear}\n")
+
+
+
+# Khởi tạo mô hình Lasso
+lasso_model = Lasso(alpha=0.01)
+
+# Huấn luyện mô hình
+lasso_model.fit(X_train, y_train)
+
+# Dự đoán trên tập kiểm tra
+y_pred_lasso = lasso_model.predict(X_test)
+
+# Đánh giá mô hình
+mse_lasso = mean_squared_error(y_test, y_pred_lasso)
+rmse_lasso = np.sqrt(mse_lasso)
+mae_lasso = mean_absolute_error(y_test, y_pred_lasso)
+r2_lasso = r2_score(y_test, y_pred_lasso)
+print(f"Lasso Regression MSE: {mse_lasso}")
+print(f"Lasso Regression R-square: {r2_lasso}\n")
+
+# Mô hình lasso ra file pkl
+
+# Khởi tạo MLPRegressor với các tham số phù hợp
+mlp_model = MLPRegressor(hidden_layer_sizes=(100,), max_iter=1000,  early_stopping=True, validation_fraction=0.1, random_state=42)
+
+# Huấn luyện mô hình
+mlp_model.fit(X_train, y_train)
+
+# Dự đoán trên tập kiểm tra
+y_pred_mlp = mlp_model.predict(X_test)
+
+# Đánh giá mô hình
+mse_mlp = mean_squared_error(y_test, y_pred_mlp)
+rmse_mlp = np.sqrt(mse_mlp)
+mae_mlp = mean_absolute_error(y_test, y_pred_mlp)
+r2_mlp = r2_score(y_test, y_pred_mlp)
+print(f"MLPRegressor MSE: {mse_mlp}")
+print(f"Lasso Regression R-square: {r2_mlp}\n")
+
+
+
+
+# Sử dụng Stacking với các mô hình Linear Regression, Lasso, và MLPRegressor
+estimators = [
+    ('linear', LinearRegression()),
+    ('lasso', Lasso(alpha=0.1)),
+    ('mlp', MLPRegressor(hidden_layer_sizes=(100,), max_iter=1000,  early_stopping=True, validation_fraction=0.1, random_state=42))
+]
+
+stacking_model = StackingRegressor(estimators=estimators, final_estimator=LinearRegression())
+
+# Huấn luyện mô hình Stacking
+stacking_model.fit(X_train, y_train)
+
+# Dự đoán trên tập kiểm tra bằng Stacking
+y_pred_stacking = stacking_model.predict(X_test)
+
+# Đánh giá mô hình Stacking
+mse_stacking = mean_squared_error(y_test, y_pred_stacking)
+rmse_stacking = np.sqrt(mse_stacking)
+mae_stacking = mean_absolute_error(y_test, y_pred_stacking)
+r2_stacking = r2_score(y_test, y_pred_stacking)
+
+print(f"Stacking Regression MSE: {mse_stacking}")
+print(f"Stacking Regression RMSE: {rmse_stacking}")
+print(f"Stacking Regression MAE: {mae_stacking}")
+print(f"Stacking Regression R-Square: {r2_stacking}")
+
+if not os.path.exists('models'):
+    os.makedirs('models')
+   
+
+
+# Giả sử bạn đã huấn luyện mô hình
+# Lưu mô hình
+joblib.dump(linear_model, 'models/linear.pkl')
+joblib.dump(lasso_model, 'models/lasso.pkl')
+joblib.dump(mlp_model, 'models/mlp.pkl')
+joblib.dump(stacking_model, 'models/stacking.pkl')
+
+file_path = 'models/linear.pkl'
+
+# Kiểm tra xem file có tồn tại không
+if os.path.exists(file_path):
+    print(f"File '{file_path}' hehe")
